ea8cca2e06ee5eb64f75cff74b5588569b0dc2eb
esctabcapslock <62400002+esctabcapslock@users.noreply.github.com>
2022-02-26 17:28:27 +0900

    Update sk.py

--- sk.py
+++ sk.py
@@ -20,7 +20,7 @@ def myrequest (domain):
 
     cmd = f'''GET / HTTP/1.1\r
 Host: {domain}\r
-User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0\r
+User-Agent: [User-Agent]\r
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r
 Accept-Language: en-US,en;q=0.5\r
 Accept-Encoding: gzip, deflate, br\r
